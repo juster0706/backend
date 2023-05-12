2b8521c59a1b89f7522d6176d9f2e28883ed216c
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-13 06:27:50 +0900

    FEAT commit  = {                                인기 계시글 찾기 기능을 api/bestposts 로 구현                                가장 likes가 많은 post 20개를                                게시글이 20개가 안될 경우에는 존재하는 게시글을 전부 출력해주는 api 제작                                 추후 likes나 views 등이 수정되거나 예상과 다른 방향으로 구성이 잡힐 경우에는 코드를 수정하겠습니다.                              };
    DOCS commit  = {
                                     post_ip로 되어있던 부분들을 형식과 약속에 맞춰 post_url 로 수정했습니다.
                                      }

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -12,7 +12,7 @@ router.post('/posts', authMiddleware, uploadImage.single('photo'), async (req, r
     try {
         const { userId, nickname } = res.locals.user;
         const { title, content, price, location } = req.body;
-        const { photo_ip } = req;
+        const { photo_url } = req;
         if (!title) {
             return res.status(412).json({ message: '게시글 제목의 형식이 일치하지 않습니다.' })
         }
@@ -25,7 +25,7 @@ router.post('/posts', authMiddleware, uploadImage.single('photo'), async (req, r
         if (!location) {
             return res.status(412).json({ message: '장소의 형식이 일치하지 않습니다.' })
         }
-        await Posts.create({ userId, nickname, title, content, photo_ip, price, location});
+        await Posts.create({ userId, nickname, title, content, photo_url, price, location});
         return res.status(200).json({ message: '게시글 작성에 성공하였습니다.' })
     } catch (err) {
         console.error(err);
@@ -33,6 +33,48 @@ router.post('/posts', authMiddleware, uploadImage.single('photo'), async (req, r
     }
 });
 
+
+router.get('/bestposts', async (req, res) => {
+    try {
+        const posts = await Posts.find();
+        const results = await Promise.all(
+            posts.map(async (item) => {
+                const post = {
+                    postId: item.postId,
+                    userId: item.userId,
+                    nickname: item.nickname,
+                    title: item.title,
+                    content: item.content,
+                    likes: item.likes,
+                    views: item.views,
+                    createdAt: item.createdAt,
+                    updatedAt: item.updatedAt,
+                    photo_url: item.photo_url,
+                };
+                const likeCount = await Likes.countDocuments({ postId: item.postId });
+                post.likeCount = likeCount;
+                return post;
+            })
+        );
+
+        if (results.length === 0) {
+            return res.status(412).json({ message: '아직 게시물이 존재하지 않습니다.' });
+        }
+
+        let bestPosts;
+        if (results.length < 20) {
+            bestPosts = results;
+        } else {
+            const sortedPosts = results.sort((a, b) => b.likeCount - a.likeCount);
+            bestPosts = sortedPosts.slice(0, 20);
+        }
+        
+        res.json(bestPosts);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send({ message: '게시글 조회에 실패하였습니다.' });
+    }
+});
 
 
 module.exports = router;
