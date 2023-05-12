decd333eaa021a105164d5500ff96b012f92a3df
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-12 19:34:36 +0900

    FEAT commit  = {                               s3를 이용하기 위한 aws key를 .env에서 불러오는 코드입니다.                              .env에 추가할 코드들은 추후 말씀드리거나, 따로 물어보시면 알려드리겠습니다.                              }

--- modules/s3.js
+++ modules/s3.js
@@ -2,3 +2,12 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
+
+
+
+require('dotenv').config();
+const s3 = new aws.S3({
+    region         : process.env.AWS_REGION,
+    accessKeyId    : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
