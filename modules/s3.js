b3f31982c6fbf2a758ad23c85dd9327379eef669
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-12 19:36:35 +0900

    FEAT commit  = {                               올라오는 사진의 허용하는 이미지 형식을 규정해주는 코드입니다.                              프론트 엔드와 상의하고 추가하거나 수정할 수 있습니다.                              }

--- modules/s3.js
+++ modules/s3.js
@@ -11,3 +11,7 @@ const s3 = new aws.S3({
     accessKeyId    : process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
+
+
+
+const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp', '.gif'];
