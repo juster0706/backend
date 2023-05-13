c3f8453f647f2a228502b99209260fa1549128db
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-13 17:23:59 +0900

    REFACTOR commit  = {                     자료구조 조금씩 삭제하고 변경했습니다.                     콘솔 로그의 위치를 옮겨주고 다음 api를 구조분해 시도하려 합니다.                                               }

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -2,7 +2,6 @@ const Posts = require('../models/posts.js');
 
 class PostRepository {
     async create(postData) {
-        console.log("postData:", postData)
         return await Posts.create(postData);
     }
 }
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,37 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
-const Posts = require('../models/posts.js');
-const Likes = require('../models/likes.js');
 const PostController = require("../controllers/posts.controller");
 const postController = new PostController();
 const uploadImage = require('../modules/s3.js');
 
 
 router.post('/posts', authMiddleware, uploadImage.single('photo'), async (req, res) => {
-    try {
-        const { userId, nickname } = res.locals.user;
-        const { title, content, price, location } = req.body;
-        const { photo_url } = req;
-        const { current_status } = false;
-        if (!title) {
-            return res.status(412).json({ message: '게시글 제목의 형식이 일치하지 않습니다.' })
-        }
-        if (!content) {
-            return res.status(412).json({ message: '게시글 내용의 형식이 일치하지 않습니다.' })
-        }
-        if (!price) {
-            return res.status(412).json({ message: '가격의 형식이 일치하지 않습니다.' })
-        }
-        if (!location) {
-            return res.status(412).json({ message: '장소의 형식이 일치하지 않습니다.' })
-        }
-        await Posts.create({ userId, nickname, title, content, photo_url, price, location, current_status});
-        return res.status(200).json({ message: '게시글 작성에 성공하였습니다.' })
-    } catch (err) {
-        console.error(err);
-        return res.status(400).json({ message: '게시글 작성에 실패하였습니다.' });
-    }
+    postController.createPost(req, res);
 });
 
 router.get('/posts', async (req, res) => {
