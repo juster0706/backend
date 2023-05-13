2ed4889718de6da9802ee2c4004c8cba8ac9b816
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-13 15:03:30 +0900

    REFACTOR commit  = {                                            파일 업로드 라우터 객체구조 분할 시범본입니다.                                            라우터에는 URL,미들웨어를 관리하고                                            컨트롤러에서는 클라이언트 상호작용, req,res 처리와, 예외처리를 진행했습니다.                                             서비스에서는 메인 서비스를                                             레퍼지토리에서는 DB와 상호작용을 하는 방식으로 자료구조를 정리하였습니다                                               }

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,7 +1,10 @@
 const Posts = require('../models/posts.js');
 
 class PostRepository {
-
+    async create(postData) {
+        console.log("postData:", postData)
+        return await Posts.create(postData);
+    }
 }
 
 module.exports = PostRepository;
--- services/post.service.js
+++ services/post.service.js
@@ -2,7 +2,10 @@ const PostRepository = require("../repositories/post.repository");
 const postRepository = new PostRepository();
 
 class PostService {
-
+    async createPost(postData) {
+        console.log("postData:", postData);
+        return await postRepository.create(postData);
+    }
 }
 
 module.exports = PostService;
