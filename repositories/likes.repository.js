5a9ae914da2fabad333c2e3bcf83356016718893
jujigithub <fallskywill@gmail.com>
2023-05-14 22:05:39 +0900

    feat: like route 임시 구현 #7

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -1,7 +1,83 @@
+const { Posts, Comments, Likes } = require("../models");
+const { Op } = require("sequelize");
+
 class LikesRepository {
-  constructor(likesModel) {
-    this.Likes = likesModel;
-  }
+  getPostLiked = async (user_id, post_id) => {
+    const getPostLikedData = await Likes.findOne({
+      where: {
+        [Op.and]: [{ post_id }, { user_id }],
+      },
+    });
+    return getPostLikedData;
+  };
+
+  getCommentLiked = async (user_id, comment_id) => {
+    const getCommentLikedData = await Likes.findOne({
+      where: {
+        [Op.and]: [{ comment_id }, { user_id }],
+      },
+    });
+    return getCommentLikedData;
+  };
+
+  postDeleteLike = async (user_id, post_id) => {
+    const deleteLikeData = await Likes.destroy({
+      where: { [Op.and]: [{ user_id }, { post_id }] },
+    });
+    return deleteLikeData;
+  };
+
+  commentDeleteLike = async (user_id, comment_id) => {
+    const deleteLikeData = await Likes.destroy({
+      where: { [Op.and]: [{ user_id }, { comment_id }] },
+    });
+    return deleteLikeData;
+  };
+
+  createLike = async (user_id, post_id, comment_id) => {
+    const createLikeData = await Likes.create({
+      user_id,
+      post_id,
+      comment_id,
+    });
+    return createLikeData;
+  };
+
+  postDecreaseLikes = async (post_id) => {
+    const findLikesData = await Posts.findOne({
+      where: { post_id },
+    });
+
+    const decreaseLikesData = await findLikesData.decrement("likes", { by: 1 });
+    return decreaseLikesData;
+  };
+
+  postIncreaseLikes = async (post_id) => {
+    const findLikesData = await Posts.findOne({
+      where: { post_id },
+    });
+
+    const increaseLikesData = await findLikesData.increment("likes", { by: 1 });
+    return increaseLikesData;
+  };
+
+  commentDecreaseLikes = async (comment_id) => {
+    const findLikesData = await Comments.findOne({
+      where: { comment_id },
+    });
+
+    const decreaseLikesData = await findLikesData.decrement("likes", { by: 1 });
+    return decreaseLikesData;
+  };
+
+  commentIncreaseLikes = async (comment_id) => {
+    const findLikesData = await Comments.findOne({
+      where: { comment_id },
+    });
+
+    const increaseLikesData = await findLikesData.increment("likes", { by: 1 });
+    return increaseLikesData;
+  };
 }
 
 module.exports = LikesRepository;
--- services/likes.service.js
+++ services/likes.service.js
@@ -1,6 +1,93 @@
-const { Likes } = require("../models/index");
+const { Likes, Posts, Comments } = require("../models/index");
 const LikesRepository = require("../repositories/likes.repository");
+// const PostsRepository = require("../repositories/posts.repository");
+const CommentsRepository = require("../repositories/comments.repository");
+const { sequelize } = require("../models");
+const { Transaction } = require("sequelize");
+
 class LikesService {
   likesRepository = new LikesRepository(Likes);
+  // postsRepository = new PostsRepository(Posts);
+  commentsRepository = new CommentsRepository(Comments);
+
+  getPostLiked = async (user_id, post_id) => {
+    const getPostLikedData = await this.likesRepository.getPostLiked(
+      user_id,
+      post_id
+    );
+    return getPostLikedData;
+  };
+
+  getCommentLiked = async (user_id, comment_id) => {
+    const getCommentLikedData = await this.likesRepository.getCommentLiked(
+      user_id,
+      comment_id
+    );
+    return getCommentLikedData;
+  };
+
+  postLikeToggle = async (user_id, post_id, likeExist) => {
+    await sequelize.transaction(
+      { isolateLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED },
+      async (t) => {
+        if (likeExist) {
+          // 2-1. 존재하면 삭제 및 decrease
+          const deleteLikeData = await this.likesRepository.postDeleteLike(
+            user_id,
+            post_id
+          );
+
+          const decreaseLikeData = await this.likesRepository.postDecreaseLikes(
+            post_id
+          );
+
+          return { message: "댓글 좋아요 취소 완료" };
+        } else {
+          // 2-2. 존재하지 않으면 생성 및 increase
+          const createLikeData = await this.likesRepository.createLike(
+            user_id,
+            post_id
+          );
+
+          const increaseLikesData =
+            await this.likesRepository.postIncreaseLikes(post_id);
+
+          return { message: "댓글 좋아요 완료" };
+        }
+      }
+    );
+  };
+
+  commentLikeToggle = async (user_id, comment_id, likeExist) => {
+    await sequelize.transaction(
+      { isolateLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED },
+      async (t) => {
+        if (likeExist) {
+          // 2-1. 존재하면 삭제 및 decrease
+          const deleteLikeData = await this.likesRepository.commentDeleteLike(
+            user_id,
+            comment_id
+          );
+
+          const decreaseLikeData =
+            await this.likesRepository.commentDecreaseLikes(comment_id);
+
+          return { message: "월드컵 취소 좋아요 완료" };
+        } else {
+          // 2-2. 존재하지 않으면 생성 및 increase
+          const createLikeData = await this.likesRepository.createLike(
+            user_id,
+            post_id,
+            comment_id
+          );
+
+          const increaseLikesData =
+            await this.likesRepository.commentIncreaseLikes(comment_id);
+
+          return { message: "월드컵 좋아요 완료" };
+        }
+      }
+    );
+  };
 }
 module.exports = LikesService;
