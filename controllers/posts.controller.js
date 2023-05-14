63c97a03e75a7f2abc7e4785071c072e147b04a1
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-15 04:32:17 +0900

    REFACTOR commit  = {                     머지 하신 부분들 중에 충돌 일어나는 부분 병합 작업했습니다.                     다음 코드가 맞는지는 모르겠지만, 이대로 병합한 코드가 더 정확한 코드 같다는 판단 하에                     제 작업을 실험하고 커밋하기 위해 임의로 머지 하였음을 알립니다.                     추후 잘못된 병합이였다면 수정 부탁드립니다.                                               }

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -29,45 +29,6 @@ class PostController {
           .json({ message: "장소의 형식이 일치하지 않습니다." });
       }
 
-<<<<<<< HEAD
-    createPost = async (req, res) => {
-        try {
-            const { userId, nickname } = res.locals.user;
-            const { title, content, price, location } = req.body;
-            const { photo_url } = req;
-        
-            if (!title) {
-                return res.status(412).json({ message: '게시글 제목의 형식이 일치하지 않습니다.' });
-            };
-            if (!content) {
-                return res.status(412).json({ message: '게시글 내용의 형식이 일치하지 않습니다.' });
-            };
-            if (!price) {
-                return res.status(412).json({ message: '가격의 형식이 일치하지 않습니다.' });
-            };
-            if (!location) {
-                return res.status(412).json({ message: '장소의 형식이 일치하지 않습니다.' });
-            };
-        
-            const postData = {
-                userId,
-                nickname,
-                title,
-                content,
-                photo_url,
-                price,
-                location,
-                current_status: false
-            };
-            await postService.createPost(postData);
-            return res.status(200).json({ message: '게시글 작성에 성공하였습니다.' });
-
-        } catch (err) {
-            console.error(err);
-            res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
-        };
-    };
-=======
       const postData = {
         userId,
         nickname,
@@ -85,7 +46,6 @@ class PostController {
       res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
     }
   };
->>>>>>> 7be275ad36440619dce1d12253aebfd16c434f73
 
   getPosts = async (req, res) => {
     try {
@@ -95,31 +55,12 @@ class PostController {
         throw new Error(posts.message);
       }
 
-<<<<<<< HEAD
-    getPosts = async (req, res) => {
-        try {
-            const posts = await postService.getPosts();
-
-            if (posts.error) { 
-                throw new Error(posts.message);
-            }
-
-            res.json(posts);
-        } catch (err) {
-            console.error(err);
-            res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
-        }
-    };
-
-
-=======
       res.json(posts);
     } catch (err) {
       console.error(err);
       res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
     }
   };
->>>>>>> 7be275ad36440619dce1d12253aebfd16c434f73
 }
 
 module.exports = PostController;
