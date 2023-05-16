3dad673b023697c8af28ba10eefaec06c3bc5b00
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-16 11:43:32 +0900

    FIX commit  = { api를 mongodb 형식에서 mysql 형식으로 변경
                                 }

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,90 +1,67 @@
-const { Posts } = require('../models');
+const { Posts, Likes } = require('../models');
+require('dotenv').config();
+
+const db = require('../models/index.js'); // MySQL 데이터베이스 연결 객체
 
 class PostRepository {
-
-
   async create(postData) {
-    return await Posts.create(postData);
-  };
+    const query = `INSERT INTO posts (title, content) VALUES ('${postData.title}', '${postData.content}')`;
+    await db.sequelize.query(query);
+  }
 
   async getPosts() {
-    return await Posts.findAll({
-        order: [
-            ['createdAt', 'DESC']
-        ],
-    });
-}
-
-
+    const query = 'SELECT * FROM posts ORDER BY created_at DESC';
+    const result = await db.sequelize.query(query);
+    return result[0];
+  }
 
   async getLikeCount(post_id) {
-    return await Likes.countDocuments({ post_id: post_id }).catch((err) => {
-      throw new Error("좋아요 수 조회에 실패하였습니다.");
-    });
-  };
+    const query = `SELECT COUNT(*) AS count FROM likes WHERE post_id = '${post_id}'`;
+    const result = await db.sequelize.query(query);
+    const count = result[0][0].count;
+    return count;
+  }
 
-  findAllPosts = async () => {
-    return await Posts.findAll();
-};
+  async findAllPosts() {
+    const query = 'SELECT * FROM posts';
+    const result = await db.sequelize.query(query);
+    return result[0];
+  }
 
-  getPostWithCounts = async (item) => {
-    const post = {
-      post_id: item.post_id,
-      user_id: item.user_id,
-      nickname: item.nickname,
-      title: item.title,
-      content: item.content,
-      likes: item.likes,
-      views: item.views,
-      price: item.price,
-      createdAt: item.createdAt,
-      updatedAt: item.updatedAt,
-      photo_url: item.photo_url,
-      current_status: item.current_status,
-    };
-  
-    const likeCount = await Likes.countDocuments({ post_id: item.post_id });
-    post.likeCount = likeCount;
-  
+  async getPostWithCounts(item) {
+    const query = `
+      SELECT p.*, COUNT(l.post_id) AS likeCount
+      FROM posts p
+      LEFT JOIN likes l ON p.post_id = l.post_id
+      WHERE p.post_id = '${item.post_id}'
+      GROUP BY p.post_id
+    `;
+    const result = await db.sequelize.query(query);
+    const post = result[0][0];
     return post;
-  };
+  }
 
-
-
-  findPostById = async (post_id) => {
-    const post = await Posts.findOneAndUpdate(
-      { _id: post_id },
-      { $inc: { views: 1 } },
-      { new: true }
-    );
+  async findPostById(post_id) {
+    const query = `SELECT * FROM posts WHERE post_id = '${post_id}'`;
+    const result = await db.sequelize.query(query);
+    const post = result[0][0];
+    await db.sequelize.query(`UPDATE posts SET views = views + 1 WHERE post_id = '${post_id}'`);
     return post;
-  };
-  
-  getLikeCount = async (post_id) => {
-    return await Likes.countDocuments({ post_id: post_id });
-  };
-  
-  updatePostById = async (post_id, title, content, price, location, photo_url) => {
-    const date = new Date();
-    await Posts.updateOne(
-      { _id: post_id },
-      {
-        $set: {
-          title: title,
-          content: content,
-          updatedAt: date,
-          photo_url: photo_url,
-          price: price,
-          location: location,
-        },
-      }
-    );
-  };
-  
-  deletePostById = async (post_id) => {
-    await Posts.deleteOne({ _id: post_id });
-  };
-  
-};
+  }
+
+  async updatePostById(post_id, title, content, price, location, photo_url) {
+    const query = `
+      UPDATE posts
+      SET title = '${title}', content = '${content}', price = '${price}',
+      location = '${location}', photo_url = '${photo_url}', updated_at = NOW()
+      WHERE post_id = '${post_id}'
+    `;
+    await db.sequelize.query(query);
+  }
+
+  async deletePostById(post_id) {
+    await db.sequelize.query(`DELETE FROM posts WHERE post_id = '${post_id}'`);
+  }
+}
 
 module.exports = PostRepository;
